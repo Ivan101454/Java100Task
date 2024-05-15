18b1d5b5d9f0397731f8875914275c6c6877d536
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-05-15 20:38:59 +0300

    add task 2 variant b

--- src/com/ivan101454/tasks/taskAandB/SumAAndB.java
+++ src/com/ivan101454/tasks/taskAandB/SumAAndB.java
@@ -2,6 +2,8 @@ package com.ivan101454.tasks.taskAandB;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -19,8 +21,25 @@ public class SumAAndB {
             a = scanner.nextInt();
             b = scanner.nextInt();
             String sum = String.valueOf(a + b);
-            bw.write(sum);
+//            bw.write(sum);
             System.out.println(sum);
+            variantB();
+        }
+    }
+
+    public static void variantB() throws IOException {
+        int sum = 0;
+        File fileIn = Path.of("resources", "input.txt").toFile();
+        File fileOut = Path.of("resources", "output.txt").toFile();
+        try (Scanner scanner = new Scanner(fileIn);
+             BufferedWriter bw = new BufferedWriter(new FileWriter(fileOut))) {
+            String strIn = scanner.nextLine();
+            String[] s = strIn.split(" ");
+            for (String str : s) {
+                sum += Integer.parseInt(str);
+            }
+            System.out.println(sum);
+            bw.write(String.valueOf(sum));
         }
     }
 }
